0a7960795e3dcdc46314a4e8fda8e81937c09907
Shanqiuzhiji <2772179523@qq.com>
2024-05-09 17:58:34 +0800

    123

--- include/main.h
+++ include/main.h
@@ -10,6 +10,9 @@ extern float angle[3];
 #include "MsTimer2.h"  
 #include <Servo.h>
 #include "Wire.h"
+#include "SoftwareSerial.h"
+#include "Ultrasonic.h"
+
 //用户头文件
 #include "MPU6050.h"
 #include "MyPID.h"
@@ -24,5 +27,5 @@ extern float angle[3];
 #define Servent_Address 0xAA
 
 void Confinguration(void) ;// 外设初始化总函数
-
+void serialEvent();
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,116 @@
 #include "main.h"
 
+// LedControl lc = LedControl(12, 11, 13, 1);
+// u8 lc1 = 0, lc2 = 0, lc3 = 0;
+
+SoftwareSerial mySerial(9, 10); // 定义D9、D10分别为RX、TX
+Ultrasonic myHC(Trig_Pin,Echo_Pin);
+
 unsigned char Re_buf[11], counter = 0;
 unsigned char sign = 0;
-float angle[3];
+static float angle[3];
+
+void Confinguration(void) // 外设初始化总函数
+{
+  // IIC_Init();
+  // HC_SR04_Pin_Init();
+  // RedLight_Pin_Init();
+  Serial.begin(115200);
+  mySerial.begin(115200);
+}
 
 void setup()
 {
   Confinguration();
-  lc.shutdown(0,false);
-  lc.setIntensity(0,8);
-  lc.clearDisplay(0);
+  // lc.shutdown(0, false);
+  // lc.setIntensity(0, 8);
+  // lc.clearDisplay(0);
 }
 void loop()
 {
   // 超声传感器检测斜坡
-  float DC=Distance_Check() ;
-  if (DC < Highdis)
+  if ( myHC.read()< Highdis)
   {
     IIC_State.u8date = 1;
     IIC_State.longdate = 0;
-    IIC_Transmission(Servent_Address, IIC_State);
-    lc.setRow(0,0,lc1++);
-    // Serial.print(DC);
+    mySerial.print(IIC_State.u8date);
+    mySerial.print(IIC_State.longdate);
+    // IIC_Transmission(Servent_Address,IIC_State);
+    // Serial.print(IIC_State.u8date,HEX);
+    // lc.setRow(0, 0, lc1++);
+    // Serial.println("i am here");
     // Serial.println();
   }
   // 隧道红外检测
   if (!Top_RedLight_value)
   {
     IIC_State.u8date = 2;
-    IIC_Transmission(Servent_Address, IIC_State);
-    lc.setRow(0,1,lc2++);
-    // Serial.print(!Top_RedLight_value);
+    IIC_State.longdate = 0;
+    mySerial.println(IIC_State.u8date);
+    mySerial.println(IIC_State.longdate);
+    // IIC_Transmission(Servent_Address, IIC_State);
+    // lc.setRow(0, 1, lc2++);
+    // Serial.println("i am here1");
     // Serial.println();
   }
-//  任务红外检测
+  //  任务红外检测
   if (!Bottom_RedLight_value)
   {
     IIC_State.u8date = 3;
-    IIC_Transmission(Servent_Address, IIC_State);
-    lc.setRow(0,2,lc3++);
-    // Serial.print(!Bottom_RedLight_value);
+    IIC_State.longdate = 0;
+    mySerial.println(IIC_State.u8date);
+    mySerial.println(IIC_State.longdate);
+    // IIC_Transmission(Servent_Address, IIC_State);
+    // lc.setRow(0, 2, lc3++);
+    // Serial.println("i am here2");
     // Serial.println();
   }
-  delay(500);
+  serialEvent();
+  delay(1);
 }
-void Confinguration(void) // 外设初始化总函数
+void serialEvent()
 {
-  IIC_Init();
-  HC_SR04_Pin_Init();
-  RedLight_Pin_Init();
-  Serial.begin(9600);
+  while(Serial.available())
+  {
+    Re_buf[counter] = (unsigned char)Serial.read();
+    if (counter == 0 && Re_buf[0] != 0x55)
+      return; // 第0号数据不是帧头
+    counter++;
+    if (counter == 11) // 接收到11个数据
+    {
+      counter = 0; // 重新赋值，准备下一帧数据的接收
+      if (Re_buf[0] == 0x55) // 检查帧头
+      {
+        switch (Re_buf[1])
+        {
+        case 0x51:
+          break;
+        case 0x52:
+          break;
+        case 0x53:
+          angle[0] = (short(Re_buf[3] << 8 | Re_buf[2])) / 32768.0 * 180;
+          angle[1] = (short(Re_buf[5] << 8 | Re_buf[4])) / 32768.0 * 180;
+          angle[2] = (short(Re_buf[7] << 8 | Re_buf[6])) / 32768.0 * 180;
+          // Serial.print("<any>:");
+          // Serial.print(angle[0]);
+          // Serial.print(",");
+          // // Serial.print(" ");
+          // Serial.print(angle[1]);
+          // Serial.print(",");
+          // // Serial.print(" ");
+          // Serial.print(angle[2]);
+          // Serial.println();
+          if(IIC_State.u8date==1)
+          {
+            IIC_Duoji.u8date=0;
+            IIC_Duoji.longdate=long (angle[2]*100);
+            mySerial.println(IIC_Duoji.u8date);
+            mySerial.println(IIC_Duoji.longdate);
+          }
+          break;
+        }
+      }
+      break;
+    }   
+  }
 }
